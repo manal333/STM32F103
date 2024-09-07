9a96796c12879c13496c332822145e8ccfcf5cd8
manal333 <142994550+manal333@users.noreply.github.com>
2024-09-07 19:33:47 +0300

    Feat : Add clock enable Macros Section to the header file "stm32f103X6.h"

--- STM32_f103c6_Drivers/inc/stm32f103x6.h
+++ STM32_f103c6_Drivers/inc/stm32f103x6.h
@@ -166,3 +166,22 @@ typedef struct {
 #define AFIO    ((AFIO_TypeDef * )AFIO_BASE)
 
 //-----------------------------------------------------------------
+
+//-*-*-*-*-*-*-*-*-*-*-*
+//clock enable Macros:
+ //-*-*-*-*-*-*-*-*-*-*-*
+
+
+
+
+
+#define RCC_GPIOA_CLK_EN()  (RCC->APB2ENR |= 1<<2)
+#define RCC_GPIOB_CLK_EN()  (RCC->APB2ENR |= 1<<3)
+#define RCC_GPIOC_CLK_EN()  (RCC->APB2ENR |= 1<<4)
+#define RCC_GPIOD_CLK_EN()  (RCC->APB2ENR |= 1<<5)
+#define RCC_GPIOE_CLK_EN()  (RCC->APB2ENR |= 1<<6)
+
+#define RCC_AFIO_CLK_EN()   (RCC->APB2ENR |= 1<<0)
+
+
+
