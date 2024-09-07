494e7503c6c817cb06ef03f06f8af9089262caea
manal333 <142994550+manal333@users.noreply.github.com>
2024-09-07 19:32:10 +0300

    Feat : Add Base addresses for Memories Section to the header file "stm32f103X6.h"

--- STM32_f103c6_Drivers/inc/stm32f103x6.h
+++ STM32_f103c6_Drivers/inc/stm32f103x6.h
@@ -15,3 +15,59 @@
 #include<stdint.h>
 #include<stdlib.h>
 
+//-----------------------------
+//Base addresses for Memories
+//-----------------------------
+
+
+
+
+#define  Flash_Memory_BASE	                     0x08000000UL
+#define  SRAM_Memory_BASE      			     	 0x20000000UL
+#define  System_Memory_BASE	                     0x1FFFF000UL
+
+#define  Peripherals_BASE	                     0x40000000UL
+
+#define  Cortex_M3_internal_peripherals_BASE	 0xE0000000UL
+
+
+//--------------------------------------------------------------------
+
+//-----------------------------
+//Base addresses for AHB Peripherals
+//-----------------------------
+
+
+
+#define RCC_BASE   0x40021000UL
+
+//-----------------------------
+//Base addresses for APB2 Peripherals
+//-----------------------------
+
+//GPIO
+
+//A,B fully included in LQFP48 Package
+#define GPIOA_BASE             0x40010800UL
+#define GPIOB_BASE             0x40010C00UL
+
+//C,D Partial included in LQFP48 Package
+#define GPIOC_BASE             0x40011000UL
+#define GPIOD_BASE             0x40011400UL
+
+//E not included in LQFP48 Package
+#define GPIOE_BASE             0x40011800UL
+
+//-------
+
+//EXTI
+#define EXTI_BASE              0x40010400UL
+
+//-------
+
+//AFIO
+#define AFIO_BASE              0x40010000UL
+//-------
+
+
+//--------------------------------------------------------------------
