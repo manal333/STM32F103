06072211095e0289aa6ebdf8e83d2bf2c9a004b5
manal333 <142994550+manal333@users.noreply.github.com>
2024-09-07 14:43:15 +0300

    Feat : Add the User type definitions  section to the header file of the GPIO driver for the STM32F103.

--- STM32_f103c6_Drivers/inc/GPIO_Driver.h
+++ STM32_f103c6_Drivers/inc/GPIO_Driver.h
@@ -13,3 +13,17 @@
 
 #include<stm32f103x6.h>
 
+//-----------------------------
+//User type definitions (structures)
+//-----------------------------
+
+typedef struct
+{
+	uint16_t GPIO_Pin_Number  ;	//Specifics the GPIO pins to be configured .
+	                            // This parameter can be a value of @ref GPIO_PINS_define
+	uint8_t GPIO_Mode         ; //Specifics the GPIO mode to be configured .
+	                            // This parameter can be a value of @ref GPIO_MODE_define
+	uint8_t GPIO_Output_Speed ; //Specifics the speed for the selected pins .
+	                            // This parameter can be a value of @ref GPIO_SPEED_define
+}GPIO_PinConfig_t;
+
